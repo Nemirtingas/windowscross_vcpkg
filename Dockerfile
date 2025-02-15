3268d6f5131590ba11b1d7ad73ca5cb961cd7b86
Nemirtingas <nemirtingas@noreply.com>
2025-02-15 14:42:51 +0100

    Added docker build script.

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+ARG CLANG_VER
+ARG UBUNTU_VER
+FROM nemirtingas_compilation_base:ubuntu${UBUNTU_VER}_clang${CLANG_VER}
+ARG MSVC_VER
+ARG WINSDK_VER
+ENV MSVC_BASE=/clang_windows_sdk/msvc
+ENV MSVC_VER=${MSVC_VER}
+ENV WINSDK_BASE=/clang_windows_sdk/winsdk
+ENV WINSDK_VER=${WINSDK_VER}
+ENV WindowsSdkDir="${WINSDK_BASE}/"
+ENV WindowsSDKVersion="${WINSDK_VER}"
+RUN cd / &&\
+    git clone https://github.com/Nemirtingas/clang-msvc-sdk clang_windows_sdk --depth=1 &&\
+    git clone https://github.com/Nemirtingas/windowscross_vcpkg "--branch=${MSVC_VER}" --depth=1 "${MSVC_BASE}_tmp" &&\
+    cd "${MSVC_BASE}" &&\
+    cat "${MSVC_BASE}_tmp/"*.tgz* | tar xz &&\
+    rm -rf "${MSVC_BASE}_tmp" &&\
+    git clone https://github.com/Nemirtingas/windowscross_vcpkg "--branch=winsdk_${WINSDK_VER}" --depth=1 "${WINSDK_BASE}_tmp" &&\
+    cd "${WINSDK_BASE}" &&\
+    cat "${WINSDK_BASE}_tmp/"*.tgz* | tar xz &&\
+    rm -rf "${WINSDK_BASE}_tmp" &&\
+    ln -s /clang_windows_sdk/powershell /usr/bin/
---
+++ build.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+cd "$(dirname "$0")"
+
+container_name=
+
+function cleanup()
+{
+  [ ! -z "${container_name}" ] && docker rm "${container_name}"
+  exit -1
+}
+
+function build_image()
+{
+    NAME="$1"
+    UBUNTU_VER="$2"
+    MSVC_VER="$3"
+    WIN_VER="$4"
+    CLANG_VER="$5"
+    VERSION="${MSVC_VER}_win${WIN_VER}_clang${CLANG_VER}"
+    docker image rm nemirtingas/${NAME}:${VERSION} --force
+    docker image rm ${NAME}:${VERSION} --force
+    container_name="$(mktemp -u XXXXXXXXXXXX)"
+    echo "Container: ${container_name}"
+    docker build --build-arg "UBUNTU_VER=${UBUNTU_VER}" --build-arg CLANG_VER=${CLANG_VER} --build-arg MSVC_VER=${MSVC_VER} --build-arg WINSDK_VER=${WIN_VER} --no-cache --rm -t ${NAME}:${VERSION} . &&
+    docker run "--name=${container_name}" ${NAME}:${VERSION} /bin/bash -c exit &&
+    docker commit -m "${NAME} image built on ubuntu${UBUNTU_VER} with ${VERSION} and clang ${CLANG_VER}" -a "Nemirtingas" "${container_name}" nemirtingas/${NAME}:${VERSION} &&
+    docker push nemirtingas/${NAME}:${VERSION} &&
+    docker rm "${container_name}"
+}
+
+trap cleanup INT
+# Start building your docker image: build_image "repository" "msvc version" "window sdk version" "clang/llvm version"
+#build_image "windowscross_vcpkg" "22.04" "msvc2019" "10.0.18362.0" "17"
+#build_image "windowscross_vcpkg" "22.04" "msvc2022_14.40.33807" "10.0.18362.0" "18"
+build_image "windowscross_vcpkg" "22.04" "msvc2022_14.40.33807" "10.0.22621.0" "20"
